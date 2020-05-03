7d1820f0d0eaf2626b8bc64cf4195c741d3ade53
Olegs Kuzicevs <61397803+upsader@users.noreply.github.com>
2020-05-03 16:51:51 +0300

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,58 @@
+let numberOfDrums = document.querySelectorAll(".drum").length;
+    
+
+for (var i = 0; i < numberOfDrums; i++) {
+
+    let allBtn = document.querySelectorAll(".drum")[i];
+        allBtn.addEventListener("click", function() {
+        let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        });   
+}
+
+document.addEventListener("keydown", function(event){
+    let pressKey = event.key;
+    makeSound(pressKey);    
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let audio = new Audio("sounds/crash.mp3");
+            audio.play(1);
+            break;
+
+        case "a":
+            let audio1 = new Audio("sounds/kick-bass.mp3");
+            audio1.play();
+            break;
+
+        case "s":
+            let audio2 = new Audio("sounds/snare.mp3");
+            audio2.play();
+            break;
+
+        case "d":
+            let audio3 = new Audio("sounds/tom-1.mp3");
+            audio3.play();
+            break;
+
+        case "j":
+            let audio4 = new Audio("sounds/tom-2.mp3");
+            audio4.play();
+            break;
+        
+        case "k":
+            let audio5 = new Audio("sounds/tom-3.mp3");
+            audio5.play();
+            break;
+
+        case "l":
+            let audio6 = new Audio("sounds/tom-4.mp3");
+            audio6.play();
+            break;
+
+        default:
+    }
+}
+
